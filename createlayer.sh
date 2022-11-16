e01a1feb1af4b5b1d92da6b83577efa1a166eae1
riazmoradian <33392093+riazmoradian@users.noreply.github.com>
2022-11-16 22:07:38 +0530

    Add support for Python 3.9
    Lambci has no support for python 3.9, use public.ecr.aws instead.

--- createlayer.sh
+++ createlayer.sh
@@ -2,7 +2,12 @@
 
 if [ "$1" != "" ] || [$# -gt 1]; then
 	echo "Creating layer compatible with python version $1"
-	docker run -v "$PWD":/var/task "lambci/lambda:build-python$1" /bin/sh -c "pip install -r requirements.txt -t python/lib/python$1/site-packages/; exit"
+	if [ "$1" == "3.9" ]; then
+		echo "Using public.ecr.aws, lambci has no support for python 3.9"
+		docker run -v "$PWD":/var/task "public.ecr.aws/sam/build-python3.9:latest" /bin/sh -c "pip install -r requirements.txt -t python/lib/python$1/site-packages/; exit"
+	else
+		docker run -v "$PWD":/var/task "lambci/lambda:build-python$1" /bin/sh -c "pip install -r requirements.txt -t python/lib/python$1/site-packages/; exit"
+	fi
 	zip -r layer.zip python > /dev/null
 	rm -r python
 	echo "Done creating layer!"
